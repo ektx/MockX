e6f838a6f2858024741c11da49af59b48d8da90b
ektx <530675800@qq.com>
2018-11-21 17:08:43 +0800

    添加注释

--- src/main/serve.js
+++ src/main/serve.js
@@ -11,11 +11,14 @@ app.get('/', (req, res) => {
 // 接受客户端启动服务器命令
 ipcMain.on('start-serve', (evt, arg) => {
     serve = app.listen(arg.port, () => {
+        // 响应用户启动服务器结果
         evt.sender.send('start-serve-result', true)
     })
 })
 
+// 接受客户端关闭服务器请求
 ipcMain.on('stop-serve', (evt, arg) => {
     serve.close()
+    // 响应关闭请求结果 
     evt.sender.send('stop-serve-result', false)
 })
